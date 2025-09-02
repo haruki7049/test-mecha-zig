60bbd4cdb34f946fd1be298a11b729d9093f29a1
haruki7049 <tontonkirikiri@gmail.com>
2025-09-02 12:06:47 +0900

    Update

--- build.zig
+++ build.zig
@@ -4,11 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const mecha = b.dependency("mecha", .{});
+
     const mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    mod.addImport("mecha", mecha.module("mecha"));
 
     const lib = b.addLibrary(.{
         .name = "test_mecha_zig",
