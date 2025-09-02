14a02b4d0b953bf3af85c7fa5aecfa4138d9188b
haruki7049 <tontonkirikiri@gmail.com>
2025-09-02 14:20:46 +0900

    Update

--- src/sexpression.zig
+++ src/sexpression.zig
@@ -3,19 +3,49 @@ const std = @import("std");
 
 const testing = std.testing;
 
+const Expression = struct {
+    const SExpression = struct {
+        values: []const Value,
+    };
+    const Word = enum {
+        Hoge,
+        Fuga,
+    };
+    const Value = union(enum) {
+        sexpr: SExpression,
+        word: Word,
+    };
+};
+
 const value = mecha.oneOf(.{
-    word,
-    sexpr,
+    word.map((struct {
+        fn f(w: Expression.Word) Expression.Value {
+            return .{ .word = w };
+        }
+    }).f),
+    sexpression.map((struct {
+        fn f(w: Expression.SExpression) Expression.Value {
+            return .{ .sexpr = w };
+        }
+    }).f),
 });
 
-fn valueRef() mecha.Parser(void) {
+fn valueRef() mecha.Parser(Expression.Value) {
     return value;
 }
 
 const word = mecha.oneOf(.{
-    token(mecha.string("hoge")),
-    token(mecha.string("fuga")),
+    wordToken(mecha.string("hoge"), .Hoge),
+    wordToken(mecha.string("fuga"), .Fuga),
 });
+
+fn wordToken(comptime parser: anytype, w: Expression.Word) mecha.Parser(Expression.Word) {
+    return mecha.combine(.{ parser, whitespace }).map((struct {
+        fn f(_: []const u8) Expression.Word {
+            return w;
+        }
+    }).f);
+}
 
 const whitespace = mecha.oneOf(.{
     mecha.ascii.char(0x20), // SPC
@@ -24,37 +54,53 @@ const whitespace = mecha.oneOf(.{
     mecha.ascii.char(0x09), // BS
 }).many(.{ .collect = false }).discard();
 
-fn token(comptime parser: anytype) mecha.Parser(void) {
+fn discardToken(comptime parser: anytype) mecha.Parser(void) {
     return mecha.combine(.{ parser.discard(), whitespace });
 }
 
 const element = mecha.ref(valueRef);
-const left_parenthesis = token(mecha.ascii.char('('));
-const right_parenthesis = token(mecha.ascii.char(')'));
-const sexpr = mecha.combine(.{
+const left_parenthesis = discardToken(mecha.ascii.char('('));
+const right_parenthesis = discardToken(mecha.ascii.char(')'));
+const sexpression = mecha.combine(.{
     whitespace,
     left_parenthesis,
 
     mecha.combine(.{ whitespace, element })
-        .many(.{ .collect = false })
-        .discard(),
+        .many(.{ .collect = true }),
 
     right_parenthesis,
-});
+}).map(mecha.toStruct(Expression.SExpression));
 
 test "sexpr" {
     const allocator = testing.allocator;
 
-    const default_value = try sexpr.parse(allocator, "( hoge )");
+    const default_value = try sexpression.parse(allocator, "( hoge )");
+    defer allocator.free(default_value.value.ok.values);
     std.debug.print("{any}\n", .{default_value});
-    try mecha.expectOk(void, 8, {}, default_value);
 
-    const two_token_value = try sexpr.parse(allocator, "( hoge fuga )");
+    const two_token_value = try sexpression.parse(allocator, "( hoge fuga )");
+    defer allocator.free(two_token_value.value.ok.values);
     std.debug.print("{any}\n", .{two_token_value});
 
-    const recursed_value = try sexpr.parse(allocator, "( hoge ( fuga ) )");
+    const recursed_value = try sexpression.parse(allocator, "( hoge ( fuga ) )");
+    defer freeValue(allocator, .{ .sexpr = recursed_value.value.ok });
     std.debug.print("{any}\n", .{recursed_value});
 
-    const more_recursed_value = try sexpr.parse(allocator, "( hoge ( ( hoge hoge ) fuga ) )");
+    const more_recursed_value = try sexpression.parse(allocator, "( hoge ( ( hoge hoge ) fuga ) )");
+    defer freeValue(allocator, .{ .sexpr = more_recursed_value.value.ok });
     std.debug.print("{any}\n", .{more_recursed_value});
+}
+
+fn freeValue(allocator: std.mem.Allocator, val: Expression.Value) void {
+    switch (val) {
+        .word => {},
+        .sexpr => {
+            const sexpr = val.sexpr;
+            // ネストされた値も再帰的に解放
+            for (sexpr.values) |v| {
+                freeValue(allocator, v);
+            }
+            allocator.free(sexpr.values);
+        },
+    }
 }
